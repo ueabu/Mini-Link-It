f4b31714d0eb6e5948d3d17dc02a9370c9adefee
ueabu <umaabu@yahoo.co.uk>
2021-10-20 10:30:46 -0700

    Fixing routing issue

--- mini-link-it-server/app/main.py
+++ mini-link-it-server/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 import firebase_admin
 from firebase_admin import db
 import os
@@ -8,23 +8,23 @@ default_app = firebase_admin.initialize_app(cred_obj,  {
 	'databaseURL': 'https://mini-link-it-default-rtdb.firebaseio.com/'
 	})
 
-# app = Flask(__name__)
-app = Flask(__name__, static_folder='./build', static_url_path='/')
-
+app = Flask(__name__, static_folder='./build/static', template_folder="./build" )
 
 @app.route("/")
 def hello_world():
-    return app.send_static_file('index.html')
+    return redirect("/app")
 
-    # return redirect('https://www.example.com')
+@app.route("/app")
+def homepage():
+    return render_template('index.html')
 
-@app.route("/<path:generatedKey>", methods=['GET'])
+@app.route('/<path:generatedKey>', methods=['GET'])
+
 def fetch_from_firebase(generatedKey):
     ref = db.reference("/"+ generatedKey)
     data = ref.get()
-
     if not data:
         return '404 not found'
     else:
         longURL = data['longURL']
-        return  redirect(longURL)
+        return redirect(longURL)
