8e10053a39ce78d14a6cf0da3aca3a0ee59423da
ueabu <umaabu@yahoo.co.uk>
2021-10-20 00:13:21 -0700

    make it better

--- mini-link-it-client/src/index.js
+++ mini-link-it-client/src/index.js
@@ -4,7 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAbo5jKpHHQK9l6q9GnqprbXufvoUjJflM",
+  authDomain: "mini-link-it.firebaseapp.com",
+  projectId: "mini-link-it",
+  storageBucket: "mini-link-it.appspot.com",
+  messagingSenderId: "49632902433",
+  appId: "1:49632902433:web:23d16c6db052a8551d9260",
+  measurementId: "G-MN3XV0B1JJ"
+};
+
+initializeApp(firebaseConfig);
 ReactDOM.render(
   <BrowserRouter>
       <App />
