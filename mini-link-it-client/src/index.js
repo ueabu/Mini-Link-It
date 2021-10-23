bdc702535dfca0026b2d2d4d6780960c30ab4bcd
ueabu <umaabu@yahoo.co.uk>
2021-10-23 02:35:09 -0500

    Updated Readme and removed some secrets

--- mini-link-it-client/src/index.js
+++ mini-link-it-client/src/index.js
@@ -7,13 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbo5jKpHHQK9l6q9GnqprbXufvoUjJflM",
-  authDomain: "mini-link-it.firebaseapp.com",
-  projectId: "mini-link-it",
-  storageBucket: "mini-link-it.appspot.com",
-  messagingSenderId: "49632902433",
-  appId: "1:49632902433:web:23d16c6db052a8551d9260",
-  measurementId: "G-MN3XV0B1JJ"
+
 };
 
 initializeApp(firebaseConfig);
